aafadd15546d2382027f02cf60c8da978add6172
angelguten <elguten@hotmail.com>
2020-09-19 22:16:37 -0300

    Add files via upload

---
+++ SobrecargaDeOperadores/Ejercicio_19/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador miSumador = new Sumador();
+            Console.WriteLine("\n La suma es\n"+miSumador.Sumar(2, 3));
+            Console.WriteLine("\nLa concatenacion es\n"+miSumador.Sumar("Angel","Ariel"));
+            Console.ReadKey();
+            Console.WriteLine("\nLa cantidad de Sumas es:\n"+(int)miSumador);
+            Console.ReadKey();
+
+            Sumador otroSumador = new Sumador();
+            Console.WriteLine("\n La suma es\n" + otroSumador.Sumar(2, 3));
+            //Console.WriteLine("\nLa concatenacion es\n"+otroSumador.Sumar("Juliane", "Meirelles"));
+            Console.ReadKey();
+            Console.WriteLine("\nLa cantidad de Sumas es:\n" + (int)otroSumador);
+            Console.ReadKey();
+
+            Console.WriteLine("\n La suma de los Sumadores es:\n" + (miSumador + otroSumador));
+            Console.WriteLine("\n La suma de los Sumadores es Identica:\n" + (miSumador | otroSumador));
+            Console.ReadKey();
+             
+        }
+    }
+}
